05eaf1b8a3ad7390033c4a1d480000b93bd848ff
wata3x <felliperocha98@gmail.com>
2016-10-31 00:12:27 -0200

    comentários das alterações

---
+++ bin/js/app.js
@@ -0,0 +1,14 @@
+var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+    SimpleGame.prototype.preload = function () {
+    };
+    SimpleGame.prototype.create = function () {
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
+    var game = new SimpleGame();
+};
+//# sourceMappingURL=app.js.map
